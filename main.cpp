a1f2733057539d6d17699364f23b24f8ab2130db
GamerPP <caltom@ashcombe.surrey.sch.uk>
2021-11-03 13:13:03 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,50 @@
 #include <iostream>
+#include <string>
+#include <bitset>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, world!\n";
+    while (true){
+        system("cls");
+        int choice;
+        cout << "Please select: " << endl
+        << "    1: Hexadecimal to Denary." << endl
+        << "    2: Hexadecimal to Binary." << endl
+        << "    3: Denary to Hexadecimal." << endl
+        << "    4: Denary to Binary" << endl
+        << "    5: Binary to Denary" << endl
+        << "    6: Binary to Hexadecimal" << endl;
+        cin >> choice;
+
+        switch (choice){
+            case 1:{
+                system("cls");
+                int num;
+                cout << "Enter your Hexadecimal value (prefix with 0x): ";
+                cin >> hex >> num;
+
+                cout << num << endl;
+                system("pause");
+
+                break;
+            }
+
+            case 2:{
+                system("cls");
+                int num;
+                cout << "Enter your Hexadecimal value (prefix with 0x): ";
+                cin >> hex >> num;
+
+                string s = bitset<64>(num).to_string();
+                while (s[0] == '0'){
+                    s.erase(0, 1);
+                }
+
+                cout << s << endl;
+                system("pause");
+
+                break;
+            }
+        }
+    }
 }
